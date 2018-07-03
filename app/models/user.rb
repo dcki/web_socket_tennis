d853ddea85c197b97d08361b5e6900a7309e870f
David Winiecki <david.winiecki@gmail.com>
2018-07-02 22:48:03 -0700

    Delete associated records when logging out destroys user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :competitors, dependent: :destroy
+  has_many :game_memberships, dependent: :destroy
+  has_many :games, through: :game_memberships
+
   validates :name, length: { in: 1..100 }
 
   def self.find_by_session(cookies)
