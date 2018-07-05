f9c9b29c1a7980bf5f18877e8daca06bb8bba06a
David Winiecki <david.winiecki@gmail.com>
2018-07-05 08:30:06 -0700

    Fix rails console in production

--- aws_script.sh
+++ aws_script.sh
@@ -2,7 +2,7 @@
 
 # This script targets Amazon Linux.
 
-set -e
+set -ex
 
 sudo yum -y update
 sudo yum -y upgrade
@@ -21,17 +21,19 @@ echo 254f1c1a79e4cc814d1e7320bc5bdd995dc57e08727d30a767664619a9c8ae5a
 #
 # If openssl-devel is not installed then attempting to install bundler (or any other gem
 # I think) will fail with an error about being unable to require openssl.
-sudo yum install -y gcc zlib-devel openssl-devel
+# readline is needed for the Rails console. I'm not sure if both packages are needed.
+sudo yum install -y gcc zlib-devel openssl-devel readline readline-devel
 
 tar xzvf ruby-2.4.4.tar.gz
 cd ruby-2.4.4/
 sudo mkdir -p /web_socket_tennis_and_dependencies/ruby
-./configure --prefix=/web_socket_tennis_and_dependencies/ruby --with-openssl-dir=/usr
-make && sudo make install
+./configure --prefix=/web_socket_tennis_and_dependencies/ruby && make && sudo make install
 sudo chown -R ec2-user:ec2-user /web_socket_tennis_and_dependencies/
 
+echo 'export PATH=/web_socket_tennis_and_dependencies/ruby/bin:$PATH' >> ~/.bash_profile
+
 PATH=/web_socket_tennis_and_dependencies/ruby/bin:$PATH
-# TODO add app's ruby to PATH
+
 gem install bundler
 
 # git is needed to clone the app.
@@ -39,7 +41,7 @@ gem install bundler
 sudo yum install -y git postgresql-devel
 
 # mini_racer needs g++, and this installs g++.
-sudo yum groupinstall "Development Tools"
+sudo yum groupinstall -y "Development Tools"
 
 cd /web_socket_tennis_and_dependencies
 git clone https://github.com/dcki/web_socket_tennis.git
